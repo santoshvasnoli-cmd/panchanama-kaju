b4be99b25a455c6de1ee9252652ddb3434b072fd
Santosh Patil <santoshvasnoli@gmail.com>
2026-08-02 20:39:35 +0530

    fixed report and width

--- kaju_panchanama.py
+++ kaju_panchanama.py
@@ -156,7 +156,8 @@ USER_CREDENTIALS = {
     "officer3": "Ramesh*123",
     "officer4": "Sudhir*123",
     "officer5": "123456",
-    "REVVPPM9601":"Vaibhav@1996"
+    "REVVPPM9601":"Vaibhav@1996",
+    "NAGESH_KALE": "Nagesh@123"
 }
 
 USER_NAMES = {
@@ -165,7 +166,8 @@ USER_NAMES = {
     "officer3": "रमेश दिनकर यादव (ग्राम महसूल अधिकारी)",
     "officer4": "सुधीर गोरे (ग्राम महसूल अधिकारी)",
     "officer5": "श्रीरंग सुतार  (कोतवाल )",
-    "REVVPPM9601": "वैभव पाटील (ग्राम महसूल अधिकारी)"
+    "REVVPPM9601": "वैभव पाटील (ग्राम महसूल अधिकारी)",
+    "NAGESH_KALE": "नागेश काळे (सहाय्यक कृषी अधिकारी)",
 }
 
 if 'logged_in' not in st.session_state:
